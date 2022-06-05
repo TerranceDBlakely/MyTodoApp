1d24b592b76fa495e52b17264ceee737fc4d00c9
Terrance <Terrancedblakely@gmail.com>
2022-06-05 14:35:49 -0400

    Update index.html, script.js and one other file

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('push').addEventListener('click', function(){
+	if(document.querySelector('.inputTask-Section input').value.length = 0){
+		window.alert('Please enter a task')
+	}
+})
