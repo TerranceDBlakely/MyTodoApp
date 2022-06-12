4d7d1917e8a675cc3e624d34eddc31dc1967361a
Terrance <Terrancedblakely@gmail.com>
2022-06-12 13:38:43 -0400

    Update delete.svg, DeleteIcon.png and three other files

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('push').addEventListener('click', function(){
-	if(document.querySelector('.inputTask-Section input').value.length = 0){
-		window.alert('Please enter a task')
-	}
-})
